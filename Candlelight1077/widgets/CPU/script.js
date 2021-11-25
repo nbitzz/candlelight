0e02a603604d2518f2565bc0b9abfc034a5ecde1
nbitzz <77242831+nbitzz@users.noreply.github.com>
2021-11-25 13:31:27 -0800

    Didn't properly upload.

--- Candlelight1077/widgets/CPU/script.js
+++
@@ -1,7 +0,0 @@
-(container,options) => {
-    
-    setInterval(() => {
-     container.innerHTML = "CPU "+window.CPU_USAGE.toString()+"%"   
-    },1000)
-    container.style["text-align"] = options.align
-}
--- Candlelight1077/widgets/Username/script.js
+++
@@ -1,8 +0,0 @@
-(container,options) => {
-    let writeAs = ""
-    if (options.styling != "None") {
-        writeAs = `${options.styling}, `
-    }
-    container.innerHTML = writeAs+window.USERNAME
-    container.style["text-align"] = options.align
-}
